5150142568bd620a6d226c52729b43c72bd991c8
Clément <clement.bourgeois7485@gmail.com>
2025-03-17 00:41:45 +0100

    add login page and register page fronts

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,8 @@ import { Routes, Route } from 'react-router';
 import MobileApp from './pages/MobileApp';
 import LandingPage from './pages/LandingPage';
 import Connection from './pages/ConnectPage';
+import RegisterPage from './pages/RegisterPage';
+import LoginPage from './pages/LoginPage';
 
 function App() {
   return (
@@ -9,6 +11,8 @@ function App() {
       <Route index element={<LandingPage />} />
       <Route path='app' element={<MobileApp />} />
       <Route path='app/login' element={<Connection />} />
+      <Route path='app/login/register' element={<RegisterPage />} />
+      <Route path='app/login/login' element={<LoginPage />} />
       <Route path='*' element={<div>404</div>} />
     </Routes>
   );
---
+++ src/pages/LoginPage.tsx
@@ -0,0 +1,12 @@
+import BackgroundCircles from '@/components/ui/BackgroundCircles';
+import Login from '@/components/Connection/Login';
+const LoginPage = () => {
+    return (
+        <div>
+               <BackgroundCircles /> 
+               <Login />
+        </div>
+    );
+};
+
+export default LoginPage;
