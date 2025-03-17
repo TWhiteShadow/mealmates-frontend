08ddc90cdd051694a7c3e82de2ca0841d922631d
Clément <clement.bourgeois7485@gmail.com>
2025-03-17 23:43:30 +0100

    login

--- src/components/Connection/Login.tsx
+++ src/components/Connection/Login.tsx
@@ -1,8 +1,33 @@
 import { cn } from "@/lib/utils";
 import { buttonVariants } from "../ui/button";
 import logo from '../../assets/MealMatesLogo.png';
+import { useState } from "react";
 
-const Register = () => {
+const Login = () => {
+  const [formData, setFormData] = useState({
+    email: "",
+    password: "",
+  });
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+  };
+
+  const handleSubmit = async (e: React.FormEvent) => {
+    e.preventDefault();
+
+    
+      const response = await fetch("http://localhost:8000/api/v1/login_check'", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(formData),
+      });
+
+      const data = await response.json();
+      localStorage.setItem("token", data.token);
+    } 
+  
+
   return (
     <div className="flex flex-col items-center justify-center h-screen bg-gray-100">
       <div className='relative flex flex-col items-center space-y-4'>
@@ -11,21 +36,25 @@ const Register = () => {
       <h2 className="text-3xl font-bold text-center mb-4 font-['Lilita_One']">
         Connectez vous
       </h2>
-      <form className="flex flex-col space-y-4 w-[500px] max-w-[90vw]">
+      <form className="flex flex-col space-y-4 w-[500px] max-w-[90vw]" onSubmit={handleSubmit}>
 
         <input
           type="email"
           name="email"
           placeholder="Mail"
           className="w-full p-3 bg-gray-300 rounded-lg"
+          value={formData.email}
+          onChange={handleChange}
         />
         <input
           type="password"
           name="password"
           placeholder="Mot de passe"
           className="w-full p-3 bg-gray-300 rounded-lg"
+          value={formData.password}
+          onChange={handleChange}
         />
-        <button className={cn(buttonVariants({ variant: 'default' }), 'w-full')}>
+        <button className={cn(buttonVariants({ variant: 'default' }), 'w-full') }>
 
           Valider
 
@@ -40,4 +69,4 @@ const Register = () => {
   );
 };
 
-export default Register;
+export default Login;
