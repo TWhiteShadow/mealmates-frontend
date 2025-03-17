7364e8df51e51aa3b99ca7ee618d50644241e4c5
TWhiteShadow <thewhiteshadowgaming@gmail.com>
2025-03-17 20:35:30 +0100

    added overflow hidden to login page and register pages

--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -2,9 +2,9 @@ import BackgroundCircles from '@/components/ui/BackgroundCircles';
 import Login from '@/components/Connection/Login';
 const LoginPage = () => {
     return (
-        <div>
-               <BackgroundCircles /> 
-               <Login />
+        <div className='h-screen relative bg-gray-100 overflow-hidden'>
+            <BackgroundCircles />
+            <Login />
         </div>
     );
 };
