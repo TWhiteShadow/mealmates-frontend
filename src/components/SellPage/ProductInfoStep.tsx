59f8069280b5673b60954233aa4769e8287e9798
fabien-design <fabienrozier60@gmail.com>
2025-06-26 11:27:18 +0200

    feat: add service fee calculation and custom tooltip for price display
    - Added `react-device-detect` dependency for responsive tooltip handling.
    - Enhanced `ProductInfoStep` component to calculate and display the amount received after service fees based on the input price.
    - Implemented `CustomTooltip` component to show tooltips differently on mobile and desktop.
    - Updated `Tooltip` component to support touch events for better mobile usability.
    - Added `VITE_SERVICE_FEES` environment variable to manage service fee rates.

--- src/components/SellPage/ProductInfoStep.tsx
+++ src/components/SellPage/ProductInfoStep.tsx
@@ -3,11 +3,14 @@ import { useAtom } from 'jotai';
 import { sellFormDataAtom } from './atoms';
 import { ProductFormData } from '@/api/Product';
 import { useUserData } from '@/api/User';
+import { Info } from 'lucide-react';
+import { CustomTooltip } from '../ui/customTooltip';
 
 const ProductInfoStep = () => {
     useUserData();
-    const { register } = useFormContext<ProductFormData>();
+    const { register, watch } = useFormContext<ProductFormData>();
     const [formData] = useAtom(sellFormDataAtom);
+    const price = watch('price');
 
     return (
         <div className="space-y-4">
@@ -44,14 +47,24 @@ const ProductInfoStep = () => {
                     Prix (€)
                 </label>
                 <input
-                    {...register('price')}
+                    {...register('price', { valueAsNumber: true })}
                     type="number"
                     step="0.01"
                     min="0"
                     className="w-full p-3 bg-white border border-gray-300 rounded-lg"
                     placeholder="Ex: 1.99"
-                    defaultValue={formData.price}
                 />
+                {price !== null && price > 0.0 && (
+                    <div className='flex gap-1 items-center mt-2'>
+                        <p className="text-xs text-gray-500">
+                            Vous serez payé {(price * (1 - Number(import.meta.env.VITE_SERVICE_FEES))).toFixed(2)}€ de ce montant après la commission.
+                        </p>
+                        <CustomTooltip 
+                        trigger={<Info className="inline-block ml-1 w-4 h-4 text-purple-dark cursor-pointer" />}
+                        content="Le montant final que vous recevrez après déduction des frais de service de MealMates."
+                        />
+                    </div>
+                )}
             </div>
         </div>
     );
